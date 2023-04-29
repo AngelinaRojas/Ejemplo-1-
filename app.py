a02763fd37140698119b484e31276d95c530adca
AngelinaRojas <132143073+AngelinaRojas@users.noreply.github.com>
2023-04-29 07:49:19 -0500

    Primer-codigo:

---
+++ app.py
@@ -0,0 +1,7 @@
+#importar Backend 
+from backend.areas import areaCirculo, areaCuadrado
+
+print("esto es una prueba")
+
+areaCirculo(100)
+areaCuadrado(100)
---
+++ backend/areas.py
@@ -0,0 +1,8 @@
+# Aca vamos a calcular diferentes áreas
+
+import math
+def areaCirculo(radio):
+    print(math.pi*radio**2)
+    
+def areaCuadrado(lado):
+    print(lado*lado)
